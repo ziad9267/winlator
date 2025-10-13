c6ba24c963a64763312b58dcca0b6931bf0059bd
Ziad <160241605+ziad9267@users.noreply.github.com>
2025-10-13 20:33:05 +0300

    Removed VirglRenderer completely

--- app/src/main/java/com/winlator/cmod/xconnector/UnixSocketConfig.java
+++ app/src/main/java/com/winlator/cmod/xconnector/UnixSocketConfig.java
@@ -9,7 +9,7 @@ public class UnixSocketConfig {
     public static final String ALSA_SERVER_PATH = "/usr/tmp/.sound/AS0";
     public static final String PULSE_SERVER_PATH = "/usr/tmp/.sound/PS0";
     public static final String XSERVER_PATH = "/usr/tmp/.X11-unix/X0";
-    public static final String VIRGL_SERVER_PATH = "/tmp/.virgl/V0";
+    //public static final String VIRGL_SERVER_PATH = "/tmp/.virgl/V0";
     public final String path;
 
     private UnixSocketConfig(String path) {
